817844767410a538bc4013df229e2f6fde7eaf8a
BayerTobias <tbayer2@gmx.de>
2023-09-21 18:22:48 +0200

    hero component

--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hero-section',
   templateUrl: './hero-section.component.html',
-  styleUrls: ['./hero-section.component.scss']
+  styleUrls: ['./hero-section.component.scss'],
 })
 export class HeroSectionComponent {
-
+  urls: string[] = [
+    'https://github.com/BayerTobias',
+    'mailto:tbayer2@gmx.de',
+    'https://www.linkedin.com/in/tobias-bayer-a01612292/',
+  ];
+  imgs: string[] = [
+    './assets/img/icons/github-icon.svg',
+    './assets/img/icons/mail-icon.svg',
+    './assets/img/icons/linkedin-icon.svg',
+  ];
 }
