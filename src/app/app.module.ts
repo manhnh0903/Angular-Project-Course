89b603a769cace2d6c571048a9944330449be8b9
BayerTobias <tbayer2@gmx.de>
2023-09-22 09:46:35 +0200

    about me component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     HeaderComponent,
-    HeroSectionComponent
+    HeroSectionComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
