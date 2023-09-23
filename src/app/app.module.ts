cdf0273445032b15ad74e8e29d75ae68f0c63afe
BayerTobias <tbayer2@gmx.de>
2023-09-23 13:21:25 +0200

    contact component improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ProjectComponent } from './project/project.component';
 import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,13 +23,10 @@ import { ContactComponent } from './contact/contact.component';
     MySkillsComponent,
     PortfolioComponent,
     ProjectComponent,
-    ContactComponent
+    ContactComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  @ViewChild('contactForm') contactForm!: NgForm;
+  sending: boolean = false;
 
+  async sendMail() {
+    this.sending = true;
+    const formData = this.getFormData();
+
+    // await fetch(
+    //   'https://tobias-bayer.developerakademie.net/portfolio/send_mail/send_mail.php',
+    //   {
+    //     method: 'POST',
+    //     body: formData,
+    //   }
+    // );
+
+    this.sending = false;
+  }
+
+  getFormData() {
+    const formData = new FormData();
+
+    formData.append('name', this.contactForm.value.name);
+    formData.append('email', this.contactForm.value.email);
+    formData.append('message', this.contactForm.value.message);
+
+    return formData;
+  }
 }
