9186aaa4faac83349c1706a8913d087f644b646f
BayerTobias <tbayer2@gmx.de>
2023-09-28 21:13:57 +0200

    more form logic

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -11,6 +11,19 @@ export class ContactComponent {
 
   sending: boolean = false;
   nameEntered: boolean = false;
+  nameFormInteraction: boolean = false;
+  emailFormInteraction: boolean = false;
+  emailIsValid: boolean = false;
+  messageFormInteraction: boolean = false;
+  privacyInteraction: boolean = false;
+  privacyAccepted: boolean = false;
+  privacyHover: boolean = false;
+
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+  formReadyToSend: boolean = false;
 
   async sendMail() {
     this.sending = true;
@@ -25,16 +38,35 @@ export class ContactComponent {
     // );
 
     this.sending = false;
-    this.nameEntered = true;
   }
 
   getFormData() {
     const formData = new FormData();
 
-    formData.append('name', this.contactForm.value.name);
-    formData.append('email', this.contactForm.value.email);
-    formData.append('message', this.contactForm.value.message);
+    formData.append('name', this.name);
+    formData.append('email', this.email);
+    formData.append('message', this.message);
 
     return formData;
   }
+
+  formInteraction(formType: string) {
+    if (formType === 'name') this.nameFormInteraction = true;
+    else if (formType === 'email') {
+      this.emailFormInteraction = true;
+      this.validateEmail();
+    } else if (formType === 'message') this.messageFormInteraction = true;
+  }
+
+  validateEmail() {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    this.emailIsValid = emailPattern.test(this.email);
+  }
+
+  togglePrivacy() {
+    this.privacyAccepted = !this.privacyAccepted;
+    console.log(this.privacyAccepted);
+  }
+
+  hoverImg() {}
 }
