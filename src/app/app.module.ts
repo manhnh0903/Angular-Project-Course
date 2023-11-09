268f15a8e041e6d801424050cfbecf63cec954d9
BayerTobias <tbayer2@gmx.de>
2023-11-09 13:30:10 +0100

    sign-up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { LoginComponent } from './components/login/login.component';
 /* import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore'; */
-import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
