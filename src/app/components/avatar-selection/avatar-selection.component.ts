245888c3661f53a8ea8fa3b22a1ed838e6f6bbf0
BayerTobias <tbayer2@gmx.de>
2023-11-09 19:56:43 +0100

    avatar selection styling

--- src/app/components/avatar-selection/avatar-selection.component.ts
+++ src/app/components/avatar-selection/avatar-selection.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-avatar-selection',
   templateUrl: './avatar-selection.component.html',
-  styleUrls: ['./avatar-selection.component.scss']
+  styleUrls: ['./avatar-selection.component.scss'],
 })
 export class AvatarSelectionComponent {
-
+  profilePictures: string[] = [
+    '0character.png',
+    '1character.png',
+    '2character.png',
+    '3character.png',
+    '4character.png',
+    '5character.png',
+  ];
 }
