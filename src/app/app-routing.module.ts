ed12182f2d651776ef74e9f6528ebb4474694694
BayerTobias <tbayer2@gmx.de>
2023-11-10 09:51:38 +0100

    forgot password component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { DialogEditProfileComponent } from './dialog-edit-profile/dialog-edit-pr
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { AvatarSelectionComponent } from './components/avatar-selection/avatar-selection.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'select-avatar', component: AvatarSelectionComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
 
   { path: 'profile', component: ProfileDialogComponent },
   { path: 'edit_user', component: DialogEditProfileComponent },
