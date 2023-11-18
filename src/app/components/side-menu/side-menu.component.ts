653deda743300e7e545aaf62b4f8993c4be8d20f
BayerTobias <tbayer2@gmx.de>
2023-11-18 12:09:36 +0100

    side menue user display

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,51 +1,46 @@
 import { AfterViewInit, Component, Input, OnInit, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateChannelComponent } from '../create-channel/create-channel.component';
-import { Firestore, collection, onSnapshot, query } from '@angular/fire/firestore';
 import { FirestoreService } from 'src/app/services/firestore.service';
-
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss']
+  styleUrls: ['./side-menu.component.scss'],
 })
 export class SideMenuComponent {
   constructor(public dialog: MatDialog, public fireService: FirestoreService) {
-    this.fireService.ifChangesOnChannels()
+    this.fireService.ifChangesOnChannels();
   }
-  firestore = inject(Firestore)
 
-  channelsClicked = true
-  PMclicked = true
-
-
+  channelsClicked = true;
+  PMclicked = true;
 
   openChannels() {
     if (this.channelsClicked == false) {
-      this.channelsClicked = true
+      this.channelsClicked = true;
     } else {
-      this.channelsClicked = false
+      this.channelsClicked = false;
     }
   }
-
 
   openPM() {
     if (this.PMclicked == false) {
-      this.PMclicked = true
+      this.PMclicked = true;
     } else {
-      this.PMclicked = false
+      this.PMclicked = false;
     }
   }
-
 
   openCreateChannelDialog(): void {
     const dialogRef = this.dialog.open(CreateChannelComponent, {
       height: '400px',
       width: '600px',
-      panelClass: 'createChannelDialog'
+      panelClass: 'createChannelDialog',
     });
   }
 
-
+  openPmChat(email) {
+    console.log(email);
+  }
 }
