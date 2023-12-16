fcb84ad31899e40db21e0806fe6cb739230d244f
BayerTobias <tbayer2@gmx.de>
2023-12-16 13:14:46 +0100

    user recipient overlay styling

--- src/app/components/pm-recipient-overview/pm-recipient-overview.component.ts
+++ src/app/components/pm-recipient-overview/pm-recipient-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirestoreService } from 'src/app/services/firestore.service';
 import { HomeNavigationService } from 'src/app/services/home-navigation.service';
 
 @Component({
@@ -7,5 +8,15 @@ import { HomeNavigationService } from 'src/app/services/home-navigation.service'
   styleUrls: ['./pm-recipient-overview.component.scss'],
 })
 export class PmRecipientOverviewComponent {
-  constructor(public homeNavService: HomeNavigationService) {}
+  constructor(
+    public homeNavService: HomeNavigationService,
+    private fireService: FirestoreService
+  ) {}
+
+  openPmChat(userId: string) {
+    this.homeNavService.pmRecipient = userId;
+    this.homeNavService.setChatPath('pm');
+    this.fireService.subscribeToPmRecipient(userId);
+    this.homeNavService.pmRecipientOverlayOpen = false;
+  }
 }
