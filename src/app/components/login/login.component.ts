cb3ad25b9241658abd3bac8f9c1858df0e3756e7
BayerTobias <tbayer2@gmx.de>
2023-12-28 18:14:50 +0100

    login code commented

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -45,13 +45,33 @@ export class LoginComponent {
     }, 3000);
   }
 
+  /**
+   * Getter method for the 'email' form control.
+   *
+   * @returns The 'email' form control.
+   */
   get email() {
     return this.loginForm.get('email');
   }
+
+  /**
+   * Getter method for the 'password' form control.
+   *
+   * @returns The 'password' form control.
+   */
   get password() {
     return this.loginForm.get('password');
   }
 
+  /**
+   * Submits the form with provided email and password for login.
+   * - Resets any previous errors.
+   * - Disables the login form during the submission process.
+   * - Attempts to log in with the provided email and password.
+   * - Handles errors if encountered during the login process.
+   * - Marks all form controls as touched if the form is invalid.
+   * - Enables the login form after the submission process.
+   */
   async submitFomrEmailAndPassword() {
     this.resetErrors();
     const email = this.email.value;
@@ -68,6 +88,16 @@ export class LoginComponent {
     this.loginForm.enable();
   }
 
+  /**
+   * Attempts to log in with the provided email and password using the authentication service.
+   * - If successful, triggers animation and routes to the next page.
+   * - Updates the user's online status after a delay of 2000 milliseconds.
+   * - Throws an error if the login attempt fails.
+   *
+   * @param email - The email used for login.
+   * @param password - The password used for login.
+   * @throws An error with a message indicating the login failure if unsuccessful.
+   */
   async login(email: string, password: string) {
     try {
       await this.authService.loginWithEmailAndPassword(email, password);
@@ -80,6 +110,11 @@ export class LoginComponent {
     }
   }
 
+  /**
+   * Handles authentication errors by updating relevant error flags based on the error message.
+   *
+   * @param err - The authentication error object.
+   */
   handleError(err: any) {
     if (err.message) {
       const errorMessage = err.message;
@@ -93,11 +128,19 @@ export class LoginComponent {
     }
   }
 
+  /**
+   * Resets error flags related to authentication.
+   */
   resetErrors() {
     this.noDabubbleUser = false;
     this.wrongPassword = false;
   }
 
+  /**
+   * Initiates the Google login process using the authentication service.
+   * After successful login, it performs additional actions such as animation,
+   * routing, and updating online status.
+   */
   async loginWithGoogle() {
     await this.authService.loginWithGoogle();
     this.animateAndRoute();
@@ -106,6 +149,11 @@ export class LoginComponent {
     }, 2000);
   }
 
+  /**
+   * Initiates a guest login process using predefined guest credentials
+   * with the authentication service. After successful login, it performs
+   * additional actions such as animation, routing, and updating online status.
+   */
   async guestLogin() {
     await this.authService.loginWithEmailAndPassword(
       'testuser@test.com',
@@ -117,6 +165,9 @@ export class LoginComponent {
     }, 2000);
   }
 
+  /**
+   * Animates the login success state and navigates to the home page.
+   */
   animateAndRoute() {
     this.loginSuccessful = true;
     setTimeout(() => {
@@ -125,6 +176,9 @@ export class LoginComponent {
     }, 800);
   }
 
+  /**
+   * Updates the online status of the current user.
+   */
   async updateOnlineStatus() {
     this.userService.user.onlineStatus = true;
 
