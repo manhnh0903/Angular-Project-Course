3b37ff7632e1ffcb93cbd03eeeda108379969af9
BayerTobias <tbayer2@gmx.de>
2023-12-28 18:48:04 +0100

    user service code commented

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,6 +10,13 @@ export class UserService {
 
   constructor(private firestoreService: FirestoreService) {}
 
+  /**
+   * Fetches user data for the specified userId by calling the FirestoreService's
+   * getLoggedInUserData method.
+   * Subscribes to the userDataObservable to receive and update the user information.
+   * @param userId - The unique identifier of the user.
+   */
+
   async getUserData(userId: string) {
     const userDataObservable = await this.firestoreService.getLogedInUserData(
       userId
