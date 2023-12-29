34eaf9328ef3b13b31e3634f14dabf0cbc1d1895
BayerTobias <tbayer2@gmx.de>
2023-12-29 17:32:04 +0100

    imprint and privacy-policy html + css channels-chat code commented

--- src/app/components/channels-chat/channels-chat.component.ts
+++ src/app/components/channels-chat/channels-chat.component.ts
@@ -5,18 +5,13 @@ import {
   ViewChild,
   inject,
 } from '@angular/core';
-import {
-  Firestore,
-  updateDoc,
-} from '@angular/fire/firestore';
+import { Firestore, updateDoc } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import { HomeNavigationService } from 'src/app/services/home-navigation.service';
 import { EditChannelComponent } from 'src/app/components/edit-channel/edit-channel.component';
-
-
 
 @Component({
   selector: 'app-channels-chat',
@@ -37,23 +32,29 @@ export class ChannelsChatComponent implements AfterViewInit {
 
   @ViewChild('inputFooter') inputFooter: ElementRef<HTMLInputElement>;
   @ViewChild('sendIcon', { static: false }) sendIcon: ElementRef;
-  @ViewChild('messagesOnChannel', { static: false }) messagesOnChannel: ElementRef;
+  @ViewChild('messagesOnChannel', { static: false })
+  messagesOnChannel: ElementRef;
   constructor(
     public fireService: FirestoreService,
     public navService: HomeNavigationService,
     public route: ActivatedRoute,
     public userService: UserService,
     private el: ElementRef,
-    public dialog: MatDialog,
+    public dialog: MatDialog
+  ) {}
 
-  ) {
-
+  ngAfterViewInit() {
+    this.el.nativeElement.addEventListener('click', (event) => {
+      if (event.target.classList.contains('addPeopleDialog')) {
+        this.closeAddPeople();
+        this.isButtonDisabled = true;
+      }
+    });
   }
 
-
-
-
-
+  /**
+   * Adds selected users to the current channel and updates the channel in Firestore.
+   */
   async addUsersToCurrentChannel() {
     const docReference = this.fireService.getDocRef(
       'channels',
@@ -66,7 +67,10 @@ export class ChannelsChatComponent implements AfterViewInit {
     });
   }
 
-
+  /**
+   * Filters users based on the user's name and whether they are already part of the current channel.
+   * Updates the 'filteredUsers' array with the matching users.
+   */
   async showFilteredUsers() {
     this.filteredUsers = this.fireService.allUsers.filter((filteredUser) => {
       let indexOfUser = this.fireService.currentChannel.users.findIndex(
@@ -80,7 +84,11 @@ export class ChannelsChatComponent implements AfterViewInit {
     });
   }
 
-
+  /**
+   * Adds a filtered user to the 'selectedUsers' array if not already included.
+   * Updates the button state based on the number of selected users.
+   * @param filteredUser - The user to be added to the selected users.
+   */
   addToSelectedUsers(filteredUser) {
     if (!this.selectedUsers.includes(filteredUser)) {
       this.selectedUsers.push(filteredUser);
@@ -91,7 +99,9 @@ export class ChannelsChatComponent implements AfterViewInit {
     }
   }
 
-
+  /**
+   * Toggles the 'addPeople' flag and updates the button color based on its state.
+   */
   openAddPeople() {
     this.buttonColor = this.isButtonDisabled ? 'gray' : 'blue';
     if (this.addPeople == false) {
@@ -101,23 +111,16 @@ export class ChannelsChatComponent implements AfterViewInit {
     }
   }
 
-
+  /**
+   * Closes the 'Add People' feature by setting the 'addPeople' flag to false.
+   */
   closeAddPeople() {
     this.addPeople = false;
   }
 
-
-  async ngAfterViewInit() {
-    this.el.nativeElement.addEventListener('click', (event) => {
-      if (event.target.classList.contains('addPeopleDialog')) {
-        this.closeAddPeople();
-        this.isButtonDisabled = true;
-      }
-    });
-
-  }
-
-
+  /**
+   * Opens the 'Edit Channel' dialog using Angular Material Dialog.
+   */
   openEditChannelDialog(): void {
     const dialogRef = this.dialog.open(EditChannelComponent, {
       height: '420px',
@@ -126,6 +129,4 @@ export class ChannelsChatComponent implements AfterViewInit {
       panelClass: 'editChannelDialog',
     });
   }
-
-
 }
