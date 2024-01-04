fe2aa7063e039b70a05ba3a922a240103f1975c8
ihortsar <128905338+ihortsar@users.noreply.github.com>
2024-01-04 20:47:51 +0100

    emoji thread

--- src/app/components/message-components/reactions/reactions.component.ts
+++ src/app/components/message-components/reactions/reactions.component.ts
@@ -6,7 +6,11 @@ import {
   Output,
   inject,
 } from '@angular/core';
-import { Firestore, loadBundle, updateDoc } from '@angular/fire/firestore';
+import {
+  DocumentReference,
+  Firestore,
+  updateDoc,
+} from '@angular/fire/firestore';
 import { FirestoreService } from '../../../services/firestore.service';
 import { Reaction } from '../../../classes/reaction.class';
 import { UserService } from '../../../services/user.service';
@@ -47,6 +51,9 @@ export class ReactionsComponent {
   }>();
   @Output() addEmojiEvent = new EventEmitter<{ emoji }>();
 
+  /**
+   * Toggles the visibility of the edit message div.
+   */
   openEditMessageDiv() {
     this.editMessage = !this.editMessage;
     this.openEdit = !this.openEdit;
@@ -56,6 +63,9 @@ export class ReactionsComponent {
     });
   }
 
+  /**
+   * Toggles the state of the emojiOpened property.
+   */
   openEmoji() {
     if (this.emojiOpened === false) {
       this.emojiOpened = true;
@@ -66,7 +76,11 @@ export class ReactionsComponent {
 
   async addEmoji(event) {
     let docReference;
-    if (this.type === 'channel' || this.type === 'thread' || this.type === 'thread-parent') {
+    if (
+      this.type === 'channel' ||
+      this.type === 'thread' ||
+      this.type === 'thread-parent'
+    ) {
       docReference = this.fireService.getDocRef(
         'channels',
         this.fireService.currentChannel.id
@@ -84,17 +98,24 @@ export class ReactionsComponent {
     let indexOfEmoji = this.currentMessage.reactions.findIndex(
       (reaction) => reaction.id === this.emoji.id
     ); //I check if the selected emoji already exists on the message
-    this.checkForEmoji(indexOfEmoji);
+
+    this.checkForEmoji(indexOfEmoji)
+
+
     //I change the selected message
     if (this.type === 'channel') {
       this.fireService.currentChannel.messages[this.index] =
         this.currentMessage;
       this.updateDoc(docReference, this.fireService.currentChannel.messages);
     }
-    if (this.type === 'thread' || this.type === 'thread-parent') {
-      this.fireService.currentChannel.messages[
-        this.indexParentMessage()
-      ] = this.currentMessage;
+    if (this.type === 'thread') {
+      this.fireService.currentChannel.messages[this.indexParentMessage()].thread[this.indexMessageOnThread()] =
+        this.currentMessage;
+      this.updateDoc(docReference, this.fireService.currentChannel.messages);
+    }
+    if (this.type === 'thread-parent') {
+      this.fireService.currentChannel.messages[this.indexParentMessage()] =
+        this.currentMessage;
       this.updateDoc(docReference, this.fireService.currentChannel.messages);
     }
     if (this.type === 'pm') {
@@ -104,6 +125,11 @@ export class ReactionsComponent {
     this.openEmoji();
   }
 
+  /**
+   * Creates a new Emoji Reaction based on the provided event.
+   * @param {object} event - The event containing emoji information.
+   *
+   */
   createEmoji(event) {
     this.emoji = new Reaction({
       id: event.emoji.id,
@@ -117,7 +143,13 @@ export class ReactionsComponent {
     });
   }
 
-  checkForEmoji(indexOfEmoji) {
+  /**
+   * Checks for the presence of an emoji in the current message's reactions.
+   * Calls different methods based on whether the emoji is present or not.
+   * @param {number} indexOfEmoji - The index of the emoji in the reactions array.
+   *  If -1, the emoji is not present in the message.
+   */
+  checkForEmoji(indexOfEmoji: number) {
     if (indexOfEmoji === -1) {
       this.ifEmojiIsNotOnMessage(indexOfEmoji);
     } else if (this.currentMessage.reactions[indexOfEmoji].counter !== 0) {
@@ -125,7 +157,16 @@ export class ReactionsComponent {
     }
   }
 
-  ifEmojiIsNotOnMessage(indexOfEmoji) {
+
+
+
+  /**
+   * Handles the scenario when the emoji is not present in the current message's reactions.
+   * Adds the emoji to the reactions array and updates user-related information.
+   * @param {number} indexOfEmoji - The index of the emoji in the reactions array.
+   * Should be -1 when calling this function.
+   */
+  ifEmojiIsNotOnMessage(indexOfEmoji: number) {
     this.currentMessage.reactions.push(this.emoji.toJSON());
     indexOfEmoji = this.currentMessage.reactions.findIndex(
       (reaction) => reaction.id === this.emoji.id
@@ -135,7 +176,12 @@ export class ReactionsComponent {
     );
   }
 
-  ifEmojiIsOnMessage(indexOfEmoji) {
+  /**
+   * Handles the scenario when the emoji is already present in the current message's reactions.
+   * Increases or decreases the counter and updates user-related information accordingly.
+   * @param {number} indexOfEmoji - The index of the emoji in the reactions array.
+   */
+  ifEmojiIsOnMessage(indexOfEmoji: number) {
     if (this.checkForUsersIdForEmoji(indexOfEmoji) === -1) {
       this.increaseCounterOfExistingEmoji(indexOfEmoji);
       this.currentMessage.reactions[indexOfEmoji].userIDs.push(
@@ -152,51 +198,85 @@ export class ReactionsComponent {
     }
   }
 
-  increaseCounterOfExistingEmoji(indexOfEmoji) {
+  /**
+   * Increases the counter of an existing emoji in the current message's reactions.
+   * @param {number} indexOfEmoji - The index of the emoji in the reactions array.
+   * @return {number} - The updated counter value of the emoji.
+   */
+  increaseCounterOfExistingEmoji(indexOfEmoji: number) {
     return this.currentMessage.reactions[indexOfEmoji].counter++;
   }
 
-  decreaseCounterOfExistingEmoji(indexOfEmoji) {
+  /**
+   * Decreases the counter of an existing emoji in the current message's reactions.
+   * @param {number} indexOfEmoji - The index of the emoji in the reactions array.
+   * @return {number} - The updated counter value of the emoji.
+   */
+  decreaseCounterOfExistingEmoji(indexOfEmoji: number) {
     return this.currentMessage.reactions[indexOfEmoji].counter--;
   }
 
-  removeEmojiIfCounter0(indexOfEmoji) {
+  /**
+   * Removes an emoji from the current message's reactions if its counter becomes 0.
+   * @param {number} indexOfEmoji - The index of the emoji in the reactions array.
+   */
+  removeEmojiIfCounter0(indexOfEmoji: number) {
     this.currentMessage.reactions.splice(indexOfEmoji, 1);
   }
 
-  checkForUsersIdForEmoji(indexOfEmoji) {
+  /**
+   * Checks for the presence of the current user's ID in the userIDs array of an emoji.
+   * @param {number} indexOfEmoji - The index of the emoji in the reactions array.
+   * @return {number} - The index of the user's ID in the userIDs array.
+   */
+  checkForUsersIdForEmoji(indexOfEmoji: number) {
     return this.currentMessage.reactions[indexOfEmoji].userIDs.findIndex(
       (id) => id === this.userService.user.userId
     );
   }
 
-  indexParentMessage() {
-    this.parentMessage = this.currentMessage
+  /**
+   * Indexes the position of the parent message in the current channel's messages array.
+   * @return {number} - The index of the parent message in the messages array.
+   */
+  indexParentMessage(): number {
+ /*    this.parentMessage = this.currentMessage; */
     if (this.type === 'thread-parent') {
-      this.parentMessage = this.currentMessage
+      this.parentMessage = this.currentMessage;
     }
-    return this.fireService.currentChannel.messages.findIndex(
-      (message) => message.id === this.parentMessage.id
-    );
+    let index = this.fireService.currentChannel.messages.findIndex(
+      (message) => message.id === this.parentMessage.id);
+    return index
   }
 
   indexMessageOnThread() {
-    return this.fireService.currentChannel.messages[
-      this.indexParentMessage()
-    ]/* .thread.findIndex((message) => message.id === this.currentMessage.id); */
-
+    let index = this.fireService.currentChannel.messages[
+      this.indexParentMessage()].thread.findIndex((message) => message.id === this.currentMessage.id);
+    return index
   }
 
-  async updateDoc(docReference, obj) {
+  /**
+   * Asynchronously updates a document using the provided Firestore document reference and object.
+   * @param {DocumentReference} docReference - The reference to the Firestore document.
+   * @param {object} obj - The object containing data to update the document.
+   * @return {Promise<void>} - A promise that resolves when the document is successfully updated.
+   */
+  async updateDoc(docReference: DocumentReference, obj: {}) {
     await updateDoc(docReference, {
       messages: obj,
     });
   }
 
+  /**
+   * Toggles the state of the openEdit property.
+   */
   openEditMessage() {
     this.openEdit = !this.openEdit;
   }
 
+  /**
+   * Initiates the thread by selecting the current message.
+   */
   startThread() {
     this.homeNav.selectMessage(this.currentMessage);
   }
