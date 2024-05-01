11a8f96a47b52189cd90c6c75ea542a057756783
BayerTobias <tbayer2@gmx.de>
2024-05-01 15:05:30 +0200

    main component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/components/login/login.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: MainComponent },
 ];
