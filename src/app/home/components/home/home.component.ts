7c83fb4aa4ccd87b4c662bd94b52e84764bd15e4
BayerTobias <tbayer2@gmx.de>
2024-05-26 16:43:58 +0200

    video card component styling

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -4,22 +4,48 @@ import { DataManagerService } from '../../services/data-manager.service';
 import { CommonModule } from '@angular/common';
 import { MenueStateService } from '../../services/menue-state.service';
 import { VideoUploadOverlayComponent } from '../video-upload-overlay/video-upload-overlay.component';
+import { VideoCardComponent } from '../video-card/video-card.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CommonModule, VideoUploadOverlayComponent],
+  imports: [
+    HeaderComponent,
+    CommonModule,
+    VideoUploadOverlayComponent,
+    VideoCardComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   public dataManager = inject(DataManagerService);
   public menueService = inject(MenueStateService);
+  public homeRoute: boolean = false;
+  public privateRoute: boolean = false;
+
+  private route = inject(ActivatedRoute);
 
   constructor() {}
 
   async ngOnInit() {
+    this.setRoute();
     await this.dataManager.getPublicVideos();
     await this.dataManager.getPrivateVideos();
+  }
+
+  setRoute() {
+    this.route.queryParams.subscribe((params) => {
+      const visibility = params['visibility'];
+
+      if (visibility === 'public') {
+        this.homeRoute = true;
+        this.privateRoute = false;
+      } else if (visibility === 'private') {
+        this.homeRoute = false;
+        this.privateRoute = true;
+      }
+    });
   }
 }
--- src/app/home/components/video-card/video-card.component.ts
+++ src/app/home/components/video-card/video-card.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Video } from '../../../models/video.model';
 
 @Component({
   selector: 'app-video-card',
   standalone: true,
   imports: [],
   templateUrl: './video-card.component.html',
-  styleUrl: './video-card.component.scss'
+  styleUrl: './video-card.component.scss',
 })
 export class VideoCardComponent {
-
+  @Input() video?: Video;
 }
